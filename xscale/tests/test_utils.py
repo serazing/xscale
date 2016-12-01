bd3ec3d575d76a2a4b3e30fcc5e2760f144a0eb2
serazing <guillaume.serazin@lgge.obs.grenoble-ujf.fr>
2016-12-01 19:43:00 +0100

    Making an inferring function for input parameters (useful for setting window and spectrum properties)

---
+++ xscale/tests/test_utils.py
@@ -0,0 +1,44 @@
+# Python 2/3 compatibility
+from __future__ import absolute_import, division, print_function
+# Pytest
+import pytest
+# Numpy
+import numpy as np
+# Xarray
+import xarray as xr
+# Xscale
+from xscale import utils
+
+# Testing array
+shape = (50, 30, 40)
+dims = ('x', 'y', 'z')
+array = xr.DataArray(np.random.random(shape), dims=dims)
+
+def test_infer_n_and_dims():
+	# Case n and dims are None -> returns all dimensions and all dimensions
+	assert utils.infer_n_and_dims(array, None, None) == (shape, dims)
+	# Case n is None and dims is defined -> returns the dimensions and the associated shape
+	assert utils.infer_n_and_dims(array, None, ('x', 'z')) == ((50, 40), ('x', 'z'))
+	# Case n is a dictionary and dims is None or whatever
+	dict_test = {'x': 10, 'y': 20}
+	assert utils.infer_n_and_dims(array, dict_test, None) == (dict_test.values(), dict_test.keys())
+	# Case n is an int and dims is None -> returns n over all dimensions
+	assert utils.infer_n_and_dims(array, 10, None) == ((10, 10, 10), dims)
+	# Case n is an int and dims is a BaseString
+	assert utils.infer_n_and_dims(array, 15, 'z') == ((15,), ('z',))
+	# Case n is an int and dims is an iterable
+	assert utils.infer_n_and_dims(array, 8, ['y', 'x']) == ((8, 8), ['y', 'x'])
+	# Case n is an iterable and dims is an iterable
+	assert utils.infer_n_and_dims(array, (8, 15), ['y', 'z']) == ((8, 15), ['y', 'z'])
+	# Case n is an iterable and dims is an iterable
+	assert utils.infer_n_and_dims(array, (8, 15), ['y', 'z']) == ((8, 15), ['y', 'z'])
+	# Test exceptions
+	# Case n is an iterable and dims is not an iterable
+	with pytest.raises(TypeError, message="Expecting TypeError"):
+		utils.infer_n_and_dims(array, (8, 15), 'y')
+	# Case n is an iterable and dims is not an iterable
+	with pytest.raises(ValueError, message="Expecting ValueError"):
+		utils.infer_n_and_dims(array, (8, 15), ['y'])
+	# Case n is not valid
+	with pytest.raises(TypeError, message="Expecting TypeError"):
+		utils.infer_n_and_dims(array, '8', ['y'])
